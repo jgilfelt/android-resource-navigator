64c835a7c9dcc9ba73e775cd76aa2f6585c9cfc1
jgilfelt <jgilfelt@gmail.com>
2013-01-23 20:38:34 +0000

    working resource target link resolution

--- extension/injected.js
+++ extension/injected.js
@@ -1,21 +1,59 @@
+var _VALUES_MAP = {
+  'string'     : { bucket:'values',             file:'strings.xml' },
+  'color'      : { bucket:'values',             file:'colors.xml' },
+  'style'      : { bucket:'values',             file:'styles.xml' },
+  'dimen'      : { bucket:'values',             file:'dimens.xml' }
+};
+
 function ext_resolve(s) {
-  console.log(s);
   var url = window.location.href;
-  var resource = s.replace('android:', '') + '.xml';
+  var resPath = s.replace('android:', '');
+  var resType = resPath.slice(0, resPath.indexOf('/'));
   var urlBase = url.slice(0, url.lastIndexOf('/res')+5);
-  
-  $.ajax({
-    url: urlBase + resource,
-    statusCode: {
-      404: function() {
-        alert("page not found");
+  var xmlUrl = urlBase + resPath + ".xml";
+  console.log(resType);
+
+  if (resType in _VALUES_MAP) {
+    // in values bucket
+    window.location.href = urlBase + _VALUES_MAP[resType].bucket + '/' + _VALUES_MAP[resType].file; // TODO resolve line + "&ext_target=" + resPath;
+  } else if (resType === 'drawable') {
+    // drawable
+	// try xml
+    ext_checkTarget(xmlUrl,
+      function() {
+        window.location.href = xmlUrl;
       },
-      200: function() {
-        window.location.href = urlBase + resource;
+      function() {
+		// try 9 patch
+        var pathHdpi = resPath.replace('drawable', 'drawable-hdpi');
+        var nineUrl = urlBase + pathHdpi + ".9.png";
+        var pngUrl = urlBase + pathHdpi + ".png";
+        ext_checkTarget(nineUrl,
+          function() {
+            window.location.href = nineUrl;
+          },
+          function() {
+            // regular png
+	        window.location.href = pngUrl;
+          }
+        );
       }
+    );
+  } else {
+    // other
+    window.location.href = xmlUrl;
+  }
+
+}
+
+function ext_checkTarget(targetUrl, functionOk, functionFail) {
+  $.ajax({
+    url: targetUrl,
+    statusCode: {
+      404: functionFail,
+      200: functionOk
     }
 //  }).done(function() {
 //    alert("done");
   });
-
 }
