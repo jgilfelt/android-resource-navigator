2ae2be7c060286dfc46c62e9c831dd42c1b9a25d
jgilfelt <jgilfelt@gmail.com>
2013-01-24 15:25:34 +0000

    attach linkify processing to DOMSubtreeModified

--- extension/res-linkify.js
+++ extension/res-linkify.js
@@ -1,16 +1,9 @@
 var _RESOURCE_ID_REGEX = "@((android:)?(anim|animator|drawable|style|color|dimen|layout|string|menu)/([A-Za-z0-9_:\.\/])*)";
 
 function linkify() {
-
-  // inject our js
-  var s = document.createElement('script');
-  s.src = chrome.extension.getURL("resolver-injected.js");
-  var s2 = document.createElement('script');
-  s2.src = chrome.extension.getURL("page-find-injected.js");
-  (document.head||document.documentElement).appendChild(s);
-  (document.head||document.documentElement).appendChild(s2);
-  s.parentNode.removeChild(s);
-  s2.parentNode.removeChild(s2);
+  
+  // remove dom listener
+  document.getElementById('slider').removeEventListener("DOMSubtreeModified", fireLinkify, false);
 
   // linkify the resource items
   var re = new RegExp();
@@ -38,16 +31,46 @@ function linkify() {
   for(var i = 0; node=nodes[i] ; i++) {
     node.parentNode.innerHTML = node.parentNode.innerHTML.replace(re, "<a href='javascript:arn_resolve(\"$1\")'>@$1</a>");
   }
-
+  
+  // add dom listener
+  document.getElementById('slider').addEventListener("DOMSubtreeModified", fireLinkify, false);
 }
+
+function injectJS() {
+  // inject our js
+  var s = document.createElement('script');
+  s.src = chrome.extension.getURL("resolver-injected.js");
+  var s2 = document.createElement('script');
+  s2.src = chrome.extension.getURL("page-find-injected.js");
+  (document.head||document.documentElement).appendChild(s);
+  (document.head||document.documentElement).appendChild(s2);
+  s.parentNode.removeChild(s);
+  s2.parentNode.removeChild(s2);
+}
+
+function doLinkify() {
+  console.debug("doLinkify fired");
+  linkify();
+}
+
+function fireLinkify() {
+  if(timeout) {
+    clearTimeout(timeout);
+  }
+  timeout = setTimeout(doLinkify, 500);
+}
+
+var timeout = null;
     
 (function() {
+  injectJS();
+  doLinkify();
+
   var url = window.location.href;
-  linkify();
   if (url.indexOf('##') > 0) {
     // search in page
     var target = url.slice(url.lastIndexOf('##')+2, url.length);
     arn_findResourceInPage(target); //page-find-injected.js
   }
-  //document.addEventListener("DOMContentLoaded", onLoad);
+
 })();
