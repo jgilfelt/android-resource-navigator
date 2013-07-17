0e93acf09eda2d2300eef07469c31ff8a077f21f
Jeff Gilfelt <jgilfelt@gmail.com>
2013-07-17 12:55:18 +0100

    revert to native OS style download notification, Chrome appears to have dropped support for HTML notifications

--- extension/downloader.js
+++ extension/downloader.js
@@ -21,15 +21,15 @@ var notification;
 
 var downloadHandler = function(info, tab) {
   
-  //notification = webkitNotifications.createNotification(
-  //  'images/logo-38.png',
-  //  'Downloading Drawable',
-  //  'This might take a few seconds...'
-  //);
-
-  notification = webkitNotifications.createHTMLNotification(
-    'download_notification.html'
+  notification = webkitNotifications.createNotification(
+    'images/logo-38.png',
+    'Downloading Drawable',
+    'This might take a few seconds...'
   );
+
+  //notification = webkitNotifications.createHTMLNotification(
+  //  'download_notification.html'
+  //);
 
   notification.show();
 
